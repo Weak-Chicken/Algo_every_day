6ca0170c3ff77710f3e4961e4b6258c0709f9032
Jon <aoppy199999@gmail.com>
2018-04-15 15:51:23 +0100

    2018-02-17

--- LeetCode/6_ZigZag_Conversion(FAILED).cpp
+++ LeetCode/6_ZigZag_Conversion(FAILED).cpp
@@ -18,7 +18,13 @@ public:
             }
             else
             {
-                
+                int j = i;
+                while (j < s.length())
+                {
+                    result.insert(result.length(), s.substr(j, 1));
+                    if (((numRows - 1) - i) * 2 + j < s.length()) result.insert(result.length(), s.substr(((numRows - 1) - i) * 2 + j, 1));
+                    j += 2 * numRows - 2;
+                }
             }
         }
         return result;
