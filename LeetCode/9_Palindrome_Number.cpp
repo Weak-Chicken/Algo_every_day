585124d4961c87a1c6ebd972ef33ede3829789d0
Jon <aoppy199999@gmail.com>
2018-04-15 18:25:09 +0100

    2018-02-18

--- LeetCode/9_Palindrome_Number.cpp
+++ LeetCode/9_Palindrome_Number.cpp
@@ -5,13 +5,19 @@ class Solution
 public:
     bool isPalindrome(int x) 
     {
+        bool find = true;
         stringstream palindromestream;
         palindromestream << x;
         string p_s;
         palindromestream >> p_s;
         for (int i = 0; i < p_s.length(); i++)
         {
-            
+            if (p_s[i] != p_s[p_s.length() - 1 - i])
+            {
+                find = false;
+                break;
+            }
         }
+        return find;
     }
 };
