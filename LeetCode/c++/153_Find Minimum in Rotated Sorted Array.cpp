99ac3b8beca05a0f76ba7c8080100d055b2b825a
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-03-15 17:14:17 +0800

    2019-02-23
    4ms-faster than 100%-time cost N/A(Belongs to competition)

---
+++ LeetCode/c++/153_Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    int findMin(vector<int>& nums) 
+    {
+        if (nums.size() == 0) return 0;
+        if (nums[0] < nums[nums.size() - 1]) return nums[0];
+        int i = 0, j = nums.size() - 1, res = nums[0];
+        while (i < j)
+        {
+            while (nums[i] > nums[j]) i++;
+            return nums[i];
+        }
+        return res;
+    }
+};
