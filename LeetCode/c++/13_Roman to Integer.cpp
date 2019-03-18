405eb8a97c22dd15aa489bcbe5d1636db2f75897
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-03-18 16:08:24 +0800

    2019-03-14
    48ms-faster than 95.49%-time cost 7mins

---
+++ LeetCode/c++/13_Roman to Integer.cpp
@@ -0,0 +1,40 @@
+class Solution 
+{
+public:
+    int romanToInt(string s) 
+    {
+        if (s.size() == 0) return 0;
+        int res = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            switch (s[i])
+            {
+                case 'I':
+                    if (i < s.size() - 1 && (s[i + 1] == 'V'|| s[i + 1] == 'X')) res--;
+                    else res += 1;
+                    break;
+                case 'V':
+                    res += 5;
+                    break;
+                case 'X':
+                    if (i < s.size() - 1 && (s[i + 1] == 'L'|| s[i + 1] == 'C')) res -= 10;
+                    else res += 10;
+                    break;
+                case 'L':
+                    res += 50;
+                    break;
+                case 'C':
+                    if (i < s.size() - 1 && (s[i + 1] == 'D'|| s[i + 1] == 'M')) res -= 100;
+                    else res += 100;
+                    break;
+                case 'D':
+                    res += 500;
+                    break;
+                case 'M':
+                    res += 1000;
+                    break;
+            }
+        }
+        return res;
+    }
+};
