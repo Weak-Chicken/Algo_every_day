ba7879a42a6ca2a1e0e1e754e6909afcefbd7881
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-04-08 15:54:07 +0800

    2019-05-31
    32ms-faster than 97.7%-time cost 44mins

--- LeetCode/c++/929_Unique Email Addresses.cpp
+++ LeetCode/c++/929_Unique Email Addresses.cpp
@@ -1,7 +1,3 @@
-#include "debug_assistant.h"
-
-using namespace std;
-
 class Solution 
 {
 public:
@@ -13,18 +9,17 @@ public:
             size_t found = emails[i].find('+'); size_t at = emails[i].find('@');
             if (found != std::string::npos) emails[i] = emails[i].substr(0, found) + emails[i].substr(at);
             found = emails[i].find('.');
+            at = emails[i].find('@');
             while (found != std::string::npos && found < at) 
             {
                 emails[i].replace(found, 1, "");
                 found = emails[i].find('.');
                 at = emails[i].find('@');
-                cout << "email | found | at | " << emails[i] << " | " << found << " | "  << at << endl; 
             }
             int j = 0;
             while (j < res.size()) {if (res[j] == emails[i]) break; j++;}
             if (j == res.size()) res.push_back(emails[i]);
         }
-        for (int i = 0; i < res.size(); i++) cout << res[i] << endl;
         return (int) (res.size());
     }
 };
--- LeetCode/c++/leetcode_cpp_vscode_env/main.cpp
+++ LeetCode/c++/leetcode_cpp_vscode_env/main.cpp
@@ -26,13 +26,19 @@ vector<vector<int>> triangle =
     {4,1,8,3}
 };
 
+vector<string> emails = 
+{
+"fg.r.u.uzj+o.pw@kziczvh.com","r.cyo.g+d.h+b.ja@tgsg.z.com","fg.r.u.uzj+o.f.d@kziczvh.com","r.cyo.g+ng.r.iq@tgsg.z.com","fg.r.u.uzj+lp.k@kziczvh.com","r.cyo.g+n.h.e+n.g@tgsg.z.com","fg.r.u.uzj+k+p.j@kziczvh.com","fg.r.u.uzj+w.y+b@kziczvh.com","r.cyo.g+x+d.c+f.t@tgsg.z.com","r.cyo.g+x+t.y.l.i@tgsg.z.com","r.cyo.g+brxxi@tgsg.z.com","r.cyo.g+z+dr.k.u@tgsg.z.com","r.cyo.g+d+l.c.n+g@tgsg.z.com","fg.r.u.uzj+vq.o@kziczvh.com","fg.r.u.uzj+uzq@kziczvh.com","fg.r.u.uzj+mvz@kziczvh.com","fg.r.u.uzj+taj@kziczvh.com","fg.r.u.uzj+fek@kziczvh.com"    
+};
+
 //===================================MAIN===================================
 int main() 
 {
     // debug_assistant_include_tester();
     Solution my_sol;
 
-    cout << my_sol.isPalindrome(s);
+    cout << my_sol.numUniqueEmails(emails);
+    
     // string res = my_sol.test();
     // cout << res;
     
