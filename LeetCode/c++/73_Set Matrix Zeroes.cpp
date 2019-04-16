0976b5986d6c98cb8370ff4239b4a2485f865253
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-04-16 15:24:07 +0800

    2019-06-15
    48ms-faster than 99.41%-time cost 33mins

---
+++ LeetCode/c++/73_Set Matrix Zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+        if (matrix.empty() || matrix[0].empty()) return;
+        int m = matrix.size(), n = matrix[0].size();
+        bool rows[m]; bool cols[n];
+        memset(rows, false, m); memset(cols, false, n);
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] == 0) {rows[i] = true; cols[j] = true;}
+        
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (rows[i] || cols[j]) matrix[i][j] = 0;
+    }
+};
